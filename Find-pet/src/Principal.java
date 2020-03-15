3f312542116afcf9fcdbe7a31423c01b9a074f9f
Ali the creator <frnaciscoacosta45@gmail.com>
2020-03-14 22:07:51 -0600

    añadiendo los archivos
    añadiendo los archivos generados por eclipse para trabajar en el proyecto

---
+++ Find-pet/src/Principal.java
@@ -0,0 +1,10 @@
+import Vistas.Login;
+
+public class Principal {
+	public static void main(String args []) {
+		System.out.print("simon");
+		
+		Login l = new Login();
+		l.setVisible(true);
+	}
+}
